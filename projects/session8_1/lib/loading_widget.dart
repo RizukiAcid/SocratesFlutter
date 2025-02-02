16f481ddc4ca15f618a10a246429332b58711c16
RizukiAcid <muhammad.majiid@binus.ac.id>
2025-02-02 07:56:47 +0700

    updating the loading animation using gradient colors

--- projects/session8_1/lib/loading_widget.dart
+++ projects/session8_1/lib/loading_widget.dart
@@ -1,12 +1,81 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
-class LoadingWidget extends StatelessWidget {
-  const LoadingWidget({super.key});
+class LoadingIndicator extends StatefulWidget {
+  final double size;
+
+  LoadingIndicator({this.size = 50});
+
+  @override
+  _LoadingIndicatorState createState() => _LoadingIndicatorState();
+}
+
+class _LoadingIndicatorState extends State<LoadingIndicator>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 1000),
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: CircularProgressIndicator(),
+    return SizedBox(
+      width: widget.size,
+      height: widget.size,
+      child: AnimatedBuilder(
+        animation: _controller,
+        builder: (context, child) {
+          return CustomPaint(
+            painter: LoadingPainter(_controller.value),
+          );
+        },
+      ),
     );
   }
+}
+
+class LoadingPainter extends CustomPainter {
+  final double progress;
+
+  LoadingPainter(this.progress);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final center = Offset(size.width / 2, size.height / 2);
+    final radius = size.width / 2;
+
+    final paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 5
+      ..shader = SweepGradient(
+        center: Alignment.center,
+        startAngle: -math.pi / 2,
+        endAngle: 3 * math.pi / 2,
+        colors: [Colors.blue, Colors.green, Colors.yellow, Colors.red],
+      ).createShader(Rect.fromCircle(center: center, radius: radius));
+
+    canvas.drawArc(
+      Rect.fromCircle(center: center, radius: radius),
+      -math.pi / 2 + progress * 2 * math.pi,
+      3 * math.pi / 2,
+      false,
+      paint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(LoadingPainter oldDelegate) =>
+      oldDelegate.progress != progress;
 }
---
+++ projects/session8_1/lib/second_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import '../loading_widget.dart';
+
+class SecondPage extends StatefulWidget {
+  @override
+  _SecondPageState createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 2), () {
+      setState(() {
+        _isLoading = false;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Page'),
+      ),
+      body: Center(
+        child: _isLoading
+            ? LoadingIndicator()
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Loading Complete!'),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('Go Back to First Page'),
+                  ),
+                ],
+              ),
+      ),
+    );
+  }
+}
