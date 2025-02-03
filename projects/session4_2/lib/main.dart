d487e8abfd2c97bf829887739db4fc6680358cb4
Rizuki <muhammad.majiid@binus.ac.id>
2025-02-03 16:51:54 +0700

    updating session4_2

--- projects/session4_2/lib/main.dart
+++ projects/session4_2/lib/main.dart
@@ -1,66 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CounterProvider(),
+      child: MyApp(),
+    ),
+  );
+}
+
+class CounterProvider extends ChangeNotifier {
+  int _counter = 0;
+
+  int get counter => _counter;
+
+  void incrementCounter() {
+    _counter++;
+    notifyListeners();
+  }
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    final GoRouter _router = GoRouter(
+      routes: [
+        GoRoute(
+          path: '/',
+          builder: (context, state) => MyHomePage(),
+        ),
+        GoRoute(
+          path: '/second',
+          builder: (context, state) => SecondPage(),
+        ),
+      ],
+    );
+
+    return MaterialApp.router(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => MyHomePage(),
-        '/second': (context) => SecondPage(),
-      },
+      routerDelegate: _router.routerDelegate,
+      routeInformationParser: _router.routeInformationParser,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Web Interaktif'),
+        title: Text('Flutter Demo Home Page'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Count: $_counter',
-              style: TextStyle(fontSize: 24),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('You have pushed the button this many times:'),
+                Consumer<CounterProvider>(
+                  builder: (context, counterProvider, child) {
+                    return AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      color: counterProvider.counter % 2 == 0 ? Colors.blue : Colors.red,
+                      padding: EdgeInsets.all(16.0),
+                      child: Text(
+                        '${counterProvider.counter}',
+                        style: Theme.of(context).textTheme.headlineMedium,
+                      ),
+                    );
+                  },
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    context.read<CounterProvider>().incrementCounter();
+                  },
+                  child: Text('Increment Counter'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    GoRouter.of(context).go('/second');
+                  },
+                  child: Text('Go to Second Page'),
+                ),
+              ],
             ),
-            ElevatedButton(
-              onPressed: _incrementCounter,
-              child: Text('Increment'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/second');
-              },
-              child: Text('Go to Second Page'),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
