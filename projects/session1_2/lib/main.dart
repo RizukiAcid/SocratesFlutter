38c481d0fb396c45ca2c49fa1b99f7e25c244673
RizukiAcid <muhammad.majiid@binus.ac.id>
2025-02-09 08:29:00 +0700

    adding session1_2

---
+++ projects/session1_2/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'my_widget_demo.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Widgets Demo'),
+      ),
+      body: MyWidgetDemo(),
+    );
+  }
+}
---
+++ projects/session1_2/lib/my_widget_demo.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class MyWidgetDemo extends StatefulWidget {
+  @override
+  _MyWidgetDemoState createState() => _MyWidgetDemoState();
+}
+
+class _MyWidgetDemoState extends State<MyWidgetDemo> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          // Text Widget
+          Container(
+            padding: EdgeInsets.all(16.0),
+            margin: EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              color: Colors.blueAccent,
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            child: Text(
+              'Hello, Flutter!',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+            ),
+          ),
+          // Image Widget
+          Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2-TVphB148wg1omRxgqXTMk9lDbLyunCmdw&s'),
+          // Button Widget
+          ElevatedButton(
+            onPressed: _incrementCounter,
+            child: Text('Click Me'),
+          ),
+          // Counter Display
+          Text(
+            'Counter: $_counter',
+            style: TextStyle(fontSize: 24),
+          ),
+          // Row Widget
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(Icons.star, color: Colors.red, size: 50),
+              Icon(Icons.star, color: Colors.green, size: 50),
+              Icon(Icons.star, color: Colors.blue, size: 50),
+            ],
+          ),
+          // Column Widget
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Column Item 1'),
+              Text('Column Item 2'),
+              Text('Column Item 3'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
