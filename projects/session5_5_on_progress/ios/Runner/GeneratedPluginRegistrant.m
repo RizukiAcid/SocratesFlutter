c35aebff2b49d7fb8e1ca630ded1f87f0babf840
RizukiAcid <muhammad.majiid@binus.ac.id>
2025-02-15 10:51:47 +0700

    merging branch

---
+++ projects/session5_5_on_progress/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<http_proxy/HttpProxyPlugin.h>)
+#import <http_proxy/HttpProxyPlugin.h>
+#else
+@import http_proxy;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [HttpProxyPlugin registerWithRegistrar:[registry registrarForPlugin:@"HttpProxyPlugin"]];
+}
+
+@end
