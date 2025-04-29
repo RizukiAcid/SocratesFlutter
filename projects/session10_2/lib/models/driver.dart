2e4ccf9d5cd445cfd205da85e032e9a83bcc4e0e
RizukiAcid <muhammad.majiid@binus.ac.id>
2025-04-29 21:29:30 +0700

    working on session10_2

--- projects/session10_2/lib/models/driver.dart
+++
@@ -1,5 +0,0 @@
-class Driver {
-  final String id, name;
-  final double latitude, longitude;
-  Driver({required this.id, required this.name, required this.latitude, required this.longitude});
-}
--- projects/session10_2/lib/models/ride_option.dart
+++
@@ -1,5 +0,0 @@
-class RideOption {
-  final String id, title, iconPath;
-  final double price;
-  RideOption({required this.id, required this.title, required this.iconPath, required this.price});
-}
